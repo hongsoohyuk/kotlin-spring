251a42097227fcf7aa2d48d946768b12987431b4
hongsoohyuk <hongsoohyuk1120@gmail.com>
2026-04-21 17:03:05 +0900

    add: board project setting

---
+++ fc-board/src/main/kotlin/com/fastcampus/fcboard/SampleController.kt
@@ -0,0 +1,19 @@
+package com.fastcampus.fcboard
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleController {
+    @GetMapping("/sample")
+    fun sample(): String {
+        return "Hello world!"
+    }
+
+    @PostMapping("/sample")
+    fun samplePost(@RequestParam name: String): String {
+        return "Hello $name!"
+    }
+}
